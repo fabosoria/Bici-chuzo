3344b5417afeab96476961d5357ecbddc7fb0958
armazonweb <fabosoria@gmail.com>
2015-06-22 14:13:34 -0500

    Nuevo main

---
+++ jquery-mobile/main.js
@@ -0,0 +1,10 @@
+renderHomeView: function() {
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+    $('body').html(html);
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+}
